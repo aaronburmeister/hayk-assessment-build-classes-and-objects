deebcb9b2c227d7a3d20e0c424764dc8ed20a6b3
aaronburmeister <f34r91@gmail.com>
2020-01-10 13:05:26 -0700

    Added needed methods - need one more building

--- app.rb
+++ app.rb
@@ -1,28 +1,56 @@
+require 'pry'
 class Building
 
-    attr_accessor(@name,@number_of_tenants)
-    attr_reader(@address)
+    attr_accessor(:name, :number_of_tenants)
+    attr_reader(:address, :number_of_floors)
 
-    def initialize(name,address,number_of_floors,number_of_tenants)
+    @@all = []
+
+    def initialize(name, address, number_of_floors, number_of_tenants)
         @name = name
         @address = address
         @number_of_floors = number_of_floors
         @number_of_tenants = number_of_tenants
+
+        @@all << self
     end
 
-    def all_buildings
+    def self.all
         # Return all buildings
+        @@all
     end
 
-    def average_number_of_tenants
+    def self.total_tenants #helper method
+        @@all.reduce(0) do |sum, building|
+            sum + building.number_of_tenants
+        end
+    end
+
+    def self.average_number_of_tenants
         # Get all buildings, sum of all tenants / # of buildings
+        (Building.total_tenants.to_f / @@all.length).round
     end
 
     def placard
         # returns name and address of building
+        placard = []
+        placard << self.name
+        placard << self.address
     end
 
-    def average_number_of_tenants_per_floor
+    def self.total_floors
+        @@all.reduce(0) do |sum, building|
+            sum + building.number_of_floors
+        end
+    end
+
+    def average_tenants_per_floor
         # Calculates and returns ^
+        @number_of_tenants.to_f / @number_of_floors
     end
 end
+
+the_hub = Building.new("The Hub", "Walnut Street", 7, 700)
+the_lab = Building.new("The Lab", "Platte Street", 5, 500)
+
+puts Building.average_tenants_per_floor
